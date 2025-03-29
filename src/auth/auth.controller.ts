4603162b145e5d71983c2cec0dfb8ae19be32e42
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2025-03-29 15:53:51 +0900

    feat: 카카오 리디렉션 (state 설정 포함) 로직 작성 완료

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { nanoid } from 'nanoid';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {
+  constructor(private readonly authService: AuthService) {}
 
+  @Get('kakao')
+  async redirectToKakao(@Res() res: Response) {
+    try {
+      const state = nanoid();
+
+      res.cookie('kakao_auth_state', state, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        path: '/',
+        maxAge: 300,
+      });
+
+      const kakaoURL = this.authService.getKakaoRedirectURL(state);
+      return res.redirect(kakaoURL);
+    } catch (err) {
+      console.error('카카오 인증 URL 생성 오류:', err);
+      return res.status(500).json({
+        message: '인증 처리 중 오류가 발생했습니다.',
+      });
+    }
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  getKakaoRedirectURL(state: string): string {
+    const KAKAO_REST_API = process.env.KAKAO_REST_API;
+    const CALLBACK_PATH = process.env.CALLBACK_PATH;
+    const FE_URI = process.env.FE_URI;
+
+    if (!KAKAO_REST_API || !CALLBACK_PATH || !FE_URI) {
+      throw new Error('ENV 체크 필요');
+    }
+
+    const REDIRECT_URI = `${FE_URI}${CALLBACK_PATH}`;
+
+    const kakaoAuthURL = new URL('https://kauth.kakao.com/oauth/authorize');
+
+    kakaoAuthURL.searchParams.append('client_id', KAKAO_REST_API);
+    kakaoAuthURL.searchParams.append('redirect_uri', REDIRECT_URI);
+    kakaoAuthURL.searchParams.append('response_type', 'code');
+    kakaoAuthURL.searchParams.append('state', state);
+
+    return kakaoAuthURL.toString();
+  }
+}
