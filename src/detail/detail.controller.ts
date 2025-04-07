996a4e7a2940f4548f93eb93699b94a96f1e8455
jungdadak <junghyun21110@gmail.com>
2025-04-07 10:06:54 +0900

    feat:detail 부분 spring 연결

--- src/detail/detail.controller.ts
+++ src/detail/detail.controller.ts
@@ -1,11 +1,24 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { DetailService } from './detail.service';
+import { SpringApiService } from '../lib/clients/spring.axios.service';
 
 @Controller('detail')
 export class DetailController {
   constructor(private readonly detailService: DetailService) {}
+
   @Get(':id')
-  getDetail(@Param('id') id: string) {
-    return this.detailService.getDetailPost(id);
+  async getDetail(@Param('id') id: string) {
+    const spring = new SpringApiService();
+    const client = spring.toSpring();
+    let data = undefined;
+
+    try {
+      const springRes = await client.get(`/api/gatherings/${id}`);
+      data = springRes.data;
+    } catch (e) {
+      console.error('글 조회 실패:', e);
+      throw new NotFoundException('게시글을 찾을 수 없습니다.');
+    }
+    return this.detailService.getDetailPost(data);
   }
 }
--- src/detail/detail.service.ts
+++ src/detail/detail.service.ts
@@ -3,19 +3,52 @@ import { mockTrendingPosts } from '../trending/mock/trending.post';
 
 @Injectable()
 export class DetailService {
-  getDetailPost(id: string) {
-    if (process.env.IS_MOCKING == 'true') {
-      const numericId = Number(id);
+  getMockPost(id: string) {
+    const numericId = Number(id);
 
-      // id가 1~5 사이일 경우 (캐러셀로 접근) -> 그대로 반환
-      const found = mockTrendingPosts.find((post) => post.id === numericId);
-      if (found) return found;
-      // 트렌딩 포스트 5개 중 랜덤으로 하나 뱉어줌
+    // id가 1~5 사이일 경우 (캐러셀로 접근) -> 그대로 반환
+    const found = mockTrendingPosts.find((post) => post.id === numericId);
+    if (found) return found;
+    // 트렌딩 포스트 5개 중 랜덤으로 하나 뱉어줌
+    if (process.env.IS_MOCKING == 'true') {
       const idx = Math.floor(Math.random() * mockTrendingPosts.length);
 
       return mockTrendingPosts[idx];
     }
-    //todo
-    return { id };
+  }
+
+  getDetailPost(data: any) {
+    return {
+      id: data.id,
+      title: data.title,
+      filter: {
+        startDate: data.startAt,
+        endDate: data.endAt,
+        deadlineDate: data.closedAt,
+        groupTheme: data.category,
+        groupSize: String(data.maxParticipants ?? 0),
+        gender: data.preferredGender ?? 'ANY',
+        age: [data.preferredAge ?? 'ALL'],
+      },
+      location: {
+        placeName: data.location,
+        latitude: data.latitude,
+        longitude: data.longitude,
+      },
+      content: data.content,
+      thumbnailUrl: data.thumbnailUrl,
+      tags: data.hashtags ?? [],
+
+      currentMembers: 0, // 서버에 없으므로 기본값
+      maxMembers: data.maxParticipants ?? 0,
+
+      userId: '',
+      userName: data.creator?.nickname ?? '',
+      profileImage: data.creator?.thumbnailUrl ?? null,
+      statusMessage: data.creator?.statusMessage ?? null,
+      userAge: data.creator?.ageGroup,
+      userGender: data.creator?.gender ?? null,
+      userRating: 4.5, //todo: 평점 api와 연결?
+    };
   }
 }
